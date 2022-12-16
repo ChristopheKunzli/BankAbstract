8d2e86794ed8ebefc4c4c64f7b56323bfcb5b48f
ChristopheKunzli <Christophe.KunzliI@cpnv.ch>
2022-12-16 15:00:39 +0100

    add common account class

---
+++ BankAbstract/CommonAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public class CommonAccount : Account
+    {
+        private float overdraftLimit;
+
+        public CommonAccount(float overdraftLimit, string reference, float balance, DateTime creationDate) : base(reference,balance, creationDate)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        public override void Withdraw(float amount)
+        {
+            if (amount > overdraftLimit) throw new OverdraftLimitHitException();
+
+            if (amount < 0) throw new AmountLowerThanZeroException();
+            if (balance < amount) throw new BalanceTooLowException();
+            balance -= amount;
+        }
+    }
+}
---
+++ BankAbstract/Exceptions/OverdraftLimitHitException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public class OverdraftLimitHitException : Exception
+    {
+        public OverdraftLimitHitException() : base ("Amount is higher than your overdraft limit") { }
+    }
+}
