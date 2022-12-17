8847713d4937bc391be6c8d0c20d99d71176e126
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-18 00:13:45 +0100

    add Empty test class for employee classes

---
+++ BankTests/EmployeeTests.cs
@@ -0,0 +1,28 @@
+﻿using BankAbstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTests
+{
+    public class EmployeeTests
+    {
+        private BankEmployee emp1;
+        private BankEmployee emp2;
+
+        [SetUp]
+        public void Setup()
+        {
+            emp1 = new HourEmployee("Paul", "Jean", new DateTime(1997,9,25), 0);
+            emp2 = new CommercialEmployee("Paul", "Jeannette", new DateTime(1997,9,25), 0);
+        }
+
+        [Test]
+        public void TestEmployee()
+        {
+            Assert.AreEqual(emp1.Name, "Paul");
+        }
+    }
+}
