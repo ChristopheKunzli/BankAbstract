22f63a21389228a9b4788475f4349ff5c59f8ff7
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 20:38:30 +0100

    add new BankEmployee absrtact class

---
+++ BankAbstract/People/Employees/BankEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public abstract class BankEmployee : Person
+    {
+        protected int department;
+
+        public int Department { get { return department; } }
+
+        public BankEmployee(string name, string firstName, DateTime birthDate, int department) : base(name, firstName, birthDate) 
+        { 
+            this.department = department;
+        }
+
+        public abstract double GetSalary();
+    }
+}
