7bae42a955c1dc42a3b395c5553786c0929c3860
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 20:18:49 +0100

    add Customer class

---
+++ BankAbstract/People/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public class Customer : Person
+    {
+        private bool isPrivate;
+        private List<Account> lstAccount;
+
+        public Customer(string name, string firstName, DateTime birthDate, bool isPrivate, List<Account> existingList = null) : base (name, firstName, birthDate )
+        {
+            this.isPrivate= isPrivate;
+
+            lstAccount = (existingList == null) ? new List<Account>() : existingList;
+        }
+
+        /// <summary>
+        /// Adds an account to a customer's account list
+        /// </summary>
+        /// <param name="account"></param>
+        /// <exception cref="AccountNotExistingException"></exception>
+        public void AddAccount(Account account)
+        {
+            if (account == null) throw new AccountNotExistingException();
+            lstAccount.Add(account);
+        }
+    }
+}
