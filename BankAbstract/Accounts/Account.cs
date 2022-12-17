49d88456d944a1418bfcf9629e8bf1e7a8d2a52b
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 19:57:05 +0100

    move account classes to a new folder

---
+++ BankAbstract/Accounts/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public abstract class Account
+    {
+        protected string reference;
+        protected float balance;
+        protected DateTime creationDate;
+
+        public string Reference { get { return reference; } }
+        public float Balance { get { return balance; } }
+        public DateTime CreationDate { get { return creationDate; } }
+
+        public Account(string reference, float balance, DateTime creationDate)
+        { 
+            this.reference = reference;
+            this.balance = balance;
+            this.creationDate = creationDate;
+        }
+
+        /// <summary>
+        /// Withdraws money from an account
+        /// </summary>
+        /// <param name="amount"></param>
+        public abstract void Withdraw(float amount);        
+
+        /// <summary>
+        /// Deposit money in an account
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <exception cref="AmountLowerThanZeroException"></exception>
+        public void Deposit(float amount)
+        {
+            if (amount < 0) throw new AmountLowerThanZeroException();
+            balance += amount;
+        }
+
+        /// <summary>
+        /// Transfer moneey from an account to another
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="acc"></param>
+        /// <exception cref="AccountNotExistingException"></exception>
+        public void Transfer(float amount, Account acc)
+        {
+            if (acc == null) throw new AccountNotExistingException();
+            Withdraw(amount);
+            acc.Deposit(amount);
+        }
+
+        public override string ToString()
+        {
+            return "ref: " + reference + ";balance: " + balance + ";creationDate: " + creationDate.ToString();
+        }
+    }
+}
---
+++ BankAbstract/Accounts/CommonAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public class CommonAccount : Account
+    {
+        private float overdraftLimit;
+
+        public CommonAccount(float overdraftLimit, string reference, float balance, DateTime creationDate) : base(reference,balance, creationDate)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        /// <summary>
+        /// Withdraw money from a common account
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <exception cref="OverdraftLimitHitException"></exception>
+        /// <exception cref="AmountLowerThanZeroException"></exception>
+        /// <exception cref="BalanceTooLowException"></exception>
+        public override void Withdraw(float amount)
+        {
+            if (amount > overdraftLimit) throw new OverdraftLimitHitException();
+
+            if (amount < 0) throw new AmountLowerThanZeroException();
+            if (balance < amount) throw new BalanceTooLowException();
+            balance -= amount;
+        }
+    }
+}
