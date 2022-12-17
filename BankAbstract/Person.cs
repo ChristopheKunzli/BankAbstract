67728bf295cf26db9b1a2308457eda3b232b363a
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 19:48:52 +0100

    add new class Person

---
+++ BankAbstract/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public abstract class Person
+    {
+        protected string name;
+        protected string firstName;
+        DateTime birthDate;
+
+        public string Name { get { return name; } }
+        public string FirstName { get { return firstName;} }
+        public DateTime DateTimeBirthDate { get { return birthDate;} }
+    }
+}
