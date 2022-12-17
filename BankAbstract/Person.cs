ce99cbac005f2869e5951d3a70a177768bb5870f
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 19:55:43 +0100

    finish base Person class

--- BankAbstract/Person.cs
+++ BankAbstract/Person.cs
@@ -15,5 +15,18 @@ namespace BankAbstract
         public string Name { get { return name; } }
         public string FirstName { get { return firstName;} }
         public DateTime DateTimeBirthDate { get { return birthDate;} }
+
+        /// <summary>
+        /// Basic constructor for a person object
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="firstName"></param>
+        /// <param name="birthDate"></param>
+        public Person (string name, string firstName, DateTime birthDate)
+        {
+            this.name = name;
+            this.firstName = firstName;
+            this.birthDate = birthDate;
+        }
     }
 }
