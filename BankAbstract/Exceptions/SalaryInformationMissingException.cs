2ac22775d533b3fc577b6857ffd5a5230c6867d4
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-17 23:47:57 +0100

    add new salary exception

---
+++ BankAbstract/Exceptions/SalaryInformationMissingException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstract
+{
+    public class SalaryInformationMissingException : Exception
+    {
+        public SalaryInformationMissingException() : base ("Some salary informations are missing") { }
+    }
+}
