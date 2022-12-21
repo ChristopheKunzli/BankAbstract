a7b858a4d1f457a67319ad54a43a7ee610da683b
ChristopheKunzli <Christophe.KunzliI@cpnv.ch>
2022-12-21 08:35:05 +0100

    add Console project to test Library

---
+++ BankAbstractConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAbstract;
+
+Console.WriteLine("Hello, World!");
+
+Customer customer = new Customer("Paul", "Jean", new DateTime(1997, 9, 25), true);
+
+CommonAccount comAcc = new CommonAccount(50, "234234", 400, DateTime.Now);
+customer.AddAccount(comAcc);
+
+SavingAccount savAcc = new SavingAccount(10, "525345", 200, DateTime.Now);
+customer.AddAccount(savAcc);
+
+comAcc.Withdraw(30);
+
+foreach(Account acc in customer.LstAccount)
+{
+    Console.WriteLine(acc.ToString());
+}
+
+
