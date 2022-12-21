8a45ee9bd9a9b3fadbc6209006f8d7154b1910e8
ChristopheKunzli <Christophe.KunzliI@cpnv.ch>
2022-12-21 08:46:20 +0100

    add tests for saving account class

---
+++ BankTests/SavingsAccountTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTests
+{
+    public class SavingsAccountTest
+    {
+        [Test]
+        public void Interest_balanceIsPositive()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", 100, DateTime.Now);
+
+            float expected = (float)30.0;
+            float actual = acc.Interest();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Interest_balanceIsZero()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", 0, DateTime.Now);
+
+            float expected = (float)0.0;
+            float actual = acc.Interest();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Interest_balanceIsNegative()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", -100, DateTime.Now);
+
+            float expected = (float)-30.0;
+            float actual = acc.Interest();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void Interest_balanceIsPositive_Integrate()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", 100, DateTime.Now);
+
+            float expected = acc.Balance + (float)30.0;
+            acc.Interest(true);
+
+            float actual = acc.Balance;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Interest_balanceIsZero_Integrate()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", 0, DateTime.Now);
+
+            float expected = acc.Balance + (float)0.0;
+            acc.Interest(true);
+
+            float actual = acc.Balance;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Interest_balanceIsNegative_Integrate()
+        {
+            SavingAccount acc = new SavingAccount(30, "1423423", -100, DateTime.Now);
+
+            float expected = acc.Balance + (float)-30.0;
+            acc.Interest(true);
+
+            float actual = acc.Balance;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
